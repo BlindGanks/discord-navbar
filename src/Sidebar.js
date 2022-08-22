b4dcfa39c525f939f1e256222f12f00f9b72c16a
blindganks <abdelhaktebak@gmail.com>
2022-08-22 13:14:43 +0100

    finished navbar

---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaFire, FaPoo } from "react-icons/fa";
+import { BsFillLightningFill, BsGearFill } from "react-icons/bs";
+function Sidebar() {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow-lg">
+      <SidebarIcon icon={<FaFire size="28" />} />
+      <SidebarIcon icon={<BsFillLightningFill size="32" />} />
+      <SidebarIcon icon={<BsGearFill size="20" />} />
+      <SidebarIcon icon={<FaPoo size="20" />} />
+    </div>
+  );
+}
+const SidebarIcon = ({ icon, text = "tooltip" }) => (
+  <div className="sidebar-icon group">
+    {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+  </div>
+);
+export default Sidebar;
